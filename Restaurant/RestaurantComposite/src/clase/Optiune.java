bc6a8db68c30d7ead0eb4b8b77156d470647e6f3
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2025-03-24 09:58:04 +0200

    Add files via upload

---
+++ Restaurant/RestaurantComposite/src/clase/Optiune.java
@@ -0,0 +1,10 @@
+package clase;
+
+public interface Optiune {
+
+    void stergeNod(Optiune optiune) throws Exception;
+    void adaugaNod(Optiune optiune) throws Exception;
+    Optiune getNod(int index) throws Exception;
+    void descriere();
+
+}
